7a470c74067a70c0ab98f91f8b6de1d91abbd07c
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-04-24 21:06:53 +0530

    changes done

--- bigquery-datasets.tf
+++ bigquery-datasets.tf
@@ -48,30 +48,30 @@ resource "google_bigquery_routine" "sp_clone_all_tables" {
 
   definition_body = <<-EOT
     DECLARE table_list ARRAY<STRING>;
-  DECLARE i INT64 DEFAULT 0;
-  DECLARE table_name STRING;
- 
-  EXECUTE IMMEDIATE FORMAT(
-    '''
-    SELECT ARRAY_AGG(table_name)
-    FROM `%s.%s.INFORMATION_SCHEMA.TABLES`
-    WHERE table_type = 'BASE TABLE'
-    ''',
-    src_project, src_dataset
-  )
-  INTO table_list;
- 
-  WHILE i < ARRAY_LENGTH(table_list) DO
-    SET table_name = table_list[OFFSET(i)];
+    DECLARE i INT64 DEFAULT 0;
+    DECLARE table_name STRING;
+    
     EXECUTE IMMEDIATE FORMAT(
-      'CREATE OR REPLACE TABLE `%s.%s.%s` CLONE `%s.%s.%s`',
-      dest_project, dest_dataset, table_name,
-      src_project, src_dataset, table_name
-    );
-    SET i = i + 1;
-  END WHILE;
- 
-END;
+        '''
+        SELECT ARRAY_AGG(table_name)
+        FROM `%s.%s.INFORMATION_SCHEMA.TABLES`
+        WHERE table_type = 'BASE TABLE'
+        ''',
+        src_project, src_dataset
+    )
+    INTO table_list;
+    
+    WHILE i < ARRAY_LENGTH(table_list) DO
+        SET table_name = table_list[OFFSET(i)];
+        EXECUTE IMMEDIATE FORMAT(
+        'CREATE OR REPLACE TABLE `%s.%s.%s` CLONE `%s.%s.%s`',
+        dest_project, dest_dataset, table_name,
+        src_project, src_dataset, table_name
+        );
+        SET i = i + 1;
+    END WHILE;
+    
+    END;
   EOT
 
   depends_on = [module.bigquery-dataset-copyjob]
