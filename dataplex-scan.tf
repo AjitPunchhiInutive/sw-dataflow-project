a0783a3b5655fb8760a340314a3c997865dd4232
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-05-20 17:56:50 +0530

    updating the file

--- dataplex-scan.tf
+++ dataplex-scan.tf
@@ -1,8 +1,8 @@
 module "data_profile_scan" {
   source     = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//dataplex-datascan?ref=main"
-  project_id = sw-dev-prj-sandbox
-  name       = customer-orders-profile
-  region     = us-east4
+  project_id = "sw-dev-prj-sandbox"
+  name       = "customer-orders-profile"
+  region     = "us-east4"
 
   data = {
     resource = "//bigquery.googleapis.com/projects/sw-dev-prj-sandbox/datasets/pubsub_gcs_dataflow/tables/historian_stream."
