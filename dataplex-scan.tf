82ae9d9117774c033ba10c8e633ee122d39d230f
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-05-20 18:25:36 +0530

    adding the file

--- dataplex-scan.tf
+++ dataplex-scan.tf
@@ -3,20 +3,19 @@ module "data_profile_scan" {
   project_id = "sw-dev-prj-sandbox"
   name       = "customer-orders-profile"
   region     = "us-east4"
-
   data = {
     resource = "//bigquery.googleapis.com/projects/sw-dev-prj-sandbox/datasets/pubsub_gcs_dataflow/tables/historian_stream"
   }
-
-  #execution_schedule = var.execution_schedule
+  # execution_schedule = "TZ=UTC 0 2 * * *"
+  credential_type           = "SERVICE_ACCOUNT"
+  execution_service_account = "dataplex-scanner@sw-dev-prj-sandbox.iam.gserviceaccount.com"
   data_profile_spec = {
     sampling_percent = 20
-    row_filter        = null
-    include_fields    = null
-    exclude_fields    = null
-    post_scan_actions = null
-
+    row_filter       = null   # null = scan all rows
+    include_fields   = null   # null = all columns
+    exclude_fields   = null   # null = no exclusions
     post_scan_actions = {
+      publish_to_bigquery = true
       bigquery_export = {
         results_table = "//bigquery.googleapis.com/projects/sw-dev-prj-sandbox/datasets/dataplex_profile_results/tables/customer_orders_profile"
       }
