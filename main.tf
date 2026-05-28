42b8ebf35d80452779e4dfd8912b0fb429ae5367
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-05-29 02:17:54 +0530

    adding file

--- main.tf
+++ main.tf
@@ -6,3 +6,16 @@
 #   source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//VPC-SC?ref=main"
 #   config          = local.config
 # }
+locals {
+  dataplex_configs = {
+    for b in flatten([for f in fileset("${path.module}/config/dataplex", "*.yaml") : yamldecode(file("${path.module}/config/dataplex/${f}"))]) : b.name => b
+  }
+}
+locals {
+  datascan_configs = {
+    for b in flatten([
+      for f in fileset("${path.module}/config/dataplex-datascan", "*.yaml") :
+      yamldecode(file("${path.module}/config/dataplex-datascan/${f}"))
+    ]) : b.name => b
+  }
+}
