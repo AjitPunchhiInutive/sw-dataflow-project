7b633f7f545e63820e2b53ab63ce7ef81e2b754f
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-06-09 20:23:12 +0530

    Updating the code

--- dataplex-lake-zone.tf
+++ dataplex-lake-zone.tf
@@ -19,31 +19,33 @@ locals {
     if can(val.project_id) && can(val.region) && can(val.zones)
   }
 
- dataplex_configs_resolved = {
-  for lake_key, lake_val in local.dataplex_configs :
-  lake_key => merge(lake_val, {
-    zones = {
-      for zone_key, zone_val in lake_val.zones :
-      zone_key => merge(zone_val, {
-        assets = {
-          for asset_key, asset_val in zone_val.assets :
-          asset_key => merge(asset_val, {
-            resource_project = try(asset_val.resource_project, lake_val.project_id)  # ← use resource_project
-          })
-        }
-      })
-    }
-  })
-}
+  dataplex_configs_resolved = {
+    for lake_key, lake_val in local.dataplex_configs :
+    lake_key => merge(lake_val, {
+      zones = {
+        for zone_key, zone_val in lake_val.zones :
+        zone_key => merge(zone_val, {
+          assets = {
+            for asset_key, asset_val in zone_val.assets :
+            asset_key => merge(asset_val, {
+              resource_project = try(asset_val.resource_project, lake_val.project_id)
+            })
+          }
+        })
+      }
+    })
+  }
 }
 
 module "dataplex" {
   for_each = local.dataplex_configs_resolved
 
-  source     = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//dataplex?ref=main"
-  name       = each.key
-  prefix     = try(each.value.prefix, "dev")
-  project_id = each.value.project_id
-  region     = each.value.region
-  zones      = each.value.zones
+  source       = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//dataplex?ref=main"
+  name         = each.value.name
+  display_name = try(each.value.display_name, null)
+  description  = try(each.value.description, null)
+  prefix       = null
+  project_id   = each.value.project_id
+  region       = each.value.region
+  zones        = each.value.zones
 }
